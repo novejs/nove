b79f8dafbedae30f56b467c165fa8dcde4c00be8
jasonelchen <jasonelchen@tencent.com>
2017-12-08 14:54:10 +0800

    add use decorator

--- src/decorators/core.ts
+++ src/decorators/core.ts
@@ -1,5 +1,6 @@
-import 'reflect-metadata';
-import { ControllerMetadataKeys } from '../constants';
+import { Middleware } from 'koa';
+import { ControllerMetadataKeys, ServiceMetadataKeys } from '../constants';
+import { getMetadata, setMetadata } from '../metadatas';
 
 /**
  * controller decorator
@@ -11,7 +12,58 @@ import { ControllerMetadataKeys } from '../constants';
  */
 export function Controller (path: string): ClassDecorator {
     return function (target) {
-        Reflect.defineMetadata(ControllerMetadataKeys.path, path, target);
+        setMetadata(target, ControllerMetadataKeys.path, path);
     }
 }
 
+/**
+ * setup middleware before controller or services
+ * @example
+ *   @UseBefore(middleware1)
+ *   class MyController {
+ *     @GET('/')
+ *     @UseBefore(middleware2)
+ *     index () {}
+ *   }
+ * 
+ * @param {Middleware[]} middlewares koa middlewares
+ */
+export function UseBefore (...middlewares: Middleware[]): ClassDecorator | MethodDecorator {
+    return function (target, propertyKey?: string, descriptor?: PropertyDescriptor) {
+        let trg = target;
+        if (descriptor) {
+            // only service has descriptor
+            trg = descriptor.value;
+        }
+
+        const mids = getMetadata(trg, ServiceMetadataKeys.beforeMiddlewares) || [];
+        mids.push(...middlewares);
+        setMetadata(trg, ServiceMetadataKeys.beforeMiddlewares, mids);
+    }
+}
+
+/**
+ * setup middleware after controller or services
+ * @example
+ *   @UseAfter(middleware1)
+ *   class MyController {
+ *     @GET('/')
+ *     @UseAfter(middleware2)
+ *     index () {}
+ *   }
+ * 
+ * @param {Middleware[]} middlewares koa middlewares
+ */
+export function UseAfter (...middlewares: Middleware[]): ClassDecorator | MethodDecorator {
+    return function (target, propertyKey?: string, descriptor?: PropertyDescriptor) {
+        let trg = target;
+        if (descriptor) {
+            // only service has descriptor
+            trg = descriptor.value;
+        }
+
+        const mids = getMetadata(trg, ServiceMetadataKeys.afterMiddlewares) || [];
+        mids.push(...middlewares);
+        setMetadata(trg, ServiceMetadataKeys.afterMiddlewares, mids);
+    }
+}

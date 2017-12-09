6499140a31e5246a496a2cfbf187198ae90659a0
jasonelchen <jasonelchen@tencent.com>
2017-12-09 16:39:57 +0800

    add app instance test

--- test/app/controllers/index.ts
+++ test/app/controllers/index.ts
@@ -1,16 +1,10 @@
 import { Controller, GET, Ctx, State } from '../../../src';
 
-@Controller('/blogs')
+@Controller('/')
 export class MyController {
 
     @GET('/')
-    async index (@Ctx() ctx: any, @State() state) {
-        this.a();
-
+    async index () {
         return { a: 1 }
-    }
-
-    a () {
-        console.log('aaaaaa');
     }
 }
